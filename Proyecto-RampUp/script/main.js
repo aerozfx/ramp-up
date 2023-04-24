4e56276831dba1c36490072c58137eb2ca5065cf
aerozfx <aerozfx@gmail.com>
2023-04-24 20:42:50 +0200

    Añadida sección contacto y corrección de mediaqueries

--- Proyecto-RampUp/script/main.js
+++ Proyecto-RampUp/script/main.js
@@ -6,14 +6,13 @@ import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js";
 const headerSelector = document.querySelector("header");
 
 const scrollTimeline = new ScrollTimeline({
-  scrollOffset: [CSS.percent(1), CSS.percent(99)],
+  scrollOffset: [CSS.px(1), CSS.px(20)],
 });
 
 headerSelector.animate(
   {
     // 0%
-    backdropFilter: ["blur(0px)", "blur(30px)"],
-    backgroundColor: ["transparent", "rgba(255, 255, 255, .7)"]
+    backgroundColor: ["transparent", "rgba(255, 255, 255, 1)"]
   },
   {
     timeline: scrollTimeline,
@@ -36,33 +35,21 @@ hamburguer.addEventListener("click", () => {
 // Script aboutme
 
 const aboutMeTitle = document.querySelector(".aboutme .title")
-const aboutMeTextSection = document.querySelector(".aboutme-info-text")
-const skills = document.querySelector(".aboutme-info-skills")
-const aboutMeImg = document.querySelector(".aboutme-info img")
 
 const setVisibleItems = () => {
-  if(getWidthClient() > 1024) {
+  if(getClientWidth() > 1024) {
     aboutMeTitle.innerText = "Habilidades";
-    aboutMeTextSection.classList.add("hidden");
-    skills.classList.add("visible");
-    aboutMeImg.classList.add("hidden");
   } else {
-    aboutMeImg.classList.remove("hidden")
-    skills.classList.remove("visible");
-    aboutMeTextSection.classList.remove("hidden")
     aboutMeTitle.innerText = "Sobre mí";
   }
 }
 
-const getWidthClient = () => {
-  return screen.width
+const getClientWidth = () => {
+  return window.innerWidth
 }
 
-window.addEventListener("resize", () => {
+
+document.addEventListener("resize", () => {
   setVisibleItems()
 })
-
-
-// Perspective transform based on mouse movement
-
 
